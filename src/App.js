3a011ad07bcb52be63183329d591a4a759cd2e0b
Melanie Mazanec <melanie.mazanec@gmail.com>
2018-09-27 18:12:38 -0400

    Hash router maybe?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import NetworkMapContainer from './NetworkMapContainer';
 import About from './About';
